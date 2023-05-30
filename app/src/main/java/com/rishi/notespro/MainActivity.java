7a654e836eb90d958188f57c625d346794ac328e
RishiGandhi4 <rgandhi805@gmail.com>
2023-05-30 17:46:59 +0530

    this is my second commit

--- app/src/main/java/com/rishi/notespro/MainActivity.java
+++ app/src/main/java/com/rishi/notespro/MainActivity.java
@@ -1,14 +1,26 @@
 package com.rishi.notespro;
 
+import static com.rishi.notespro.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addNoteBtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        addNoteBtn = findViewById(id.add_note_btn);
+
+        addNoteBtn.setOnClickListener((v) -> startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)));
     }
 }
---
+++ app/src/main/java/com/rishi/notespro/NoteDetailsActivity.java
@@ -0,0 +1,65 @@
+package com.rishi.notespro;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+    EditText titleEditText,contentEditText;
+    ImageButton saveNoteBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+
+        saveNoteBtn.setOnClickListener((v) -> saveNote());
+    }
+
+    void saveNote(){
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+
+        if(noteTitle == null || noteTitle.isEmpty() ){
+            titleEditText.setError("Title is Required");
+            return;
+        }
+
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    void saveNoteToFirebase(Note note){
+        DocumentReference documentReference;
+        documentReference = utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is added
+                    utility.showToast(NoteDetailsActivity.this,"Note added successfully");
+                    finish();
+                }else{
+                    utility.showToast(NoteDetailsActivity.this,"Failed while adding notes");
+                }
+            }
+        });
+    }
+}
