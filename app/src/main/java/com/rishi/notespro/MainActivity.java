6cce0a1c724bea9793db2dfc032b857dea1505a7
RishiGandhi4 <rgandhi805@gmail.com>
2023-06-01 19:47:10 +0530

    Final commit (ready to use)

--- app/src/main/java/com/rishi/notespro/MainActivity.java
+++ app/src/main/java/com/rishi/notespro/MainActivity.java
@@ -3,15 +3,26 @@ package com.rishi.notespro;
 import static com.rishi.notespro.R.*;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -20,7 +31,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(layout.activity_main);
 
         addNoteBtn = findViewById(id.add_note_btn);
+        recyclerView = findViewById(id.recyler_view);
+        menuBtn = findViewById(id.menu_btn);
 
         addNoteBtn.setOnClickListener((v) -> startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)));
+        menuBtn.setOnClickListener((v) -> showMenu());
+        setupRecyclerView();
+    }
+
+    void showMenu(){
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getTitle()=="Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    void setupRecyclerView(){
+        Query query = utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class).build();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/rishi/notespro/NoteDetailsActivity.java
+++ app/src/main/java/com/rishi/notespro/NoteDetailsActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,6 +17,10 @@ import com.google.firebase.firestore.DocumentReference;
 public class NoteDetailsActivity extends AppCompatActivity {
     EditText titleEditText,contentEditText;
     ImageButton saveNoteBtn;
+    TextView pageTitleTextView;
+    String title,content,docId;
+    boolean isEditMode = false;
+    TextView deleteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,29 @@ public class NoteDetailsActivity extends AppCompatActivity {
         titleEditText = findViewById(R.id.notes_title_text);
         contentEditText = findViewById(R.id.notes_content_text);
         saveNoteBtn = findViewById(R.id.save_note_btn);
+        pageTitleTextView = findViewById(R.id.page_title);
+        deleteBtn = findViewById(R.id.delete_note_text_view_btn);
+
+        //receive data
+        title = getIntent().getStringExtra("title");
+        content = getIntent().getStringExtra("content");
+        docId = getIntent().getStringExtra("docId");
+
+        if (docId != null && !docId.isEmpty()){
+            isEditMode = true;
+        }
+
+        titleEditText.setText(title);
+        contentEditText.setText(content);
+        if (isEditMode){
+            pageTitleTextView.setText("Edit your Note");
+            deleteBtn.setVisibility(View.VISIBLE);
+        }
+
 
         saveNoteBtn.setOnClickListener((v) -> saveNote());
+
+        deleteBtn.setOnClickListener((v)-> deleteNoteFromFirebase());
     }
 
     void saveNote(){
@@ -47,7 +74,13 @@ public class NoteDetailsActivity extends AppCompatActivity {
 
     void saveNoteToFirebase(Note note){
         DocumentReference documentReference;
-        documentReference = utility.getCollectionReferenceForNotes().document();
+        if (isEditMode){
+            //u[date the note
+            documentReference = utility.getCollectionReferenceForNotes().document(docId);
+        }else {
+            //create a new note
+            documentReference = utility.getCollectionReferenceForNotes().document();
+        }
 
         documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -58,6 +91,22 @@ public class NoteDetailsActivity extends AppCompatActivity {
                     finish();
                 }else{
                     utility.showToast(NoteDetailsActivity.this,"Failed while adding notes");
+                }
+            }
+        });
+    }
+    void deleteNoteFromFirebase(){
+        DocumentReference documentReference;
+            documentReference = utility.getCollectionReferenceForNotes().document(docId);
+        documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is deleted
+                    utility.showToast(NoteDetailsActivity.this,"Note deleted successfully");
+                    finish();
+                }else{
+                    utility.showToast(NoteDetailsActivity.this,"Failed while deleting notes");
                 }
             }
         });
